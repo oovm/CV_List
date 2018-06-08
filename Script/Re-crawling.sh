567614a62edf3ccdb5c3df6fb278d05e0614a693
GalAster <galaster@foxmail.com>
2018-06-08 13:10:28 +0800

    :sparkles:add ICCV now

--- Script/Re-crawling.sh
+++ Script/Re-crawling.sh
@@ -5,3 +5,8 @@ wolframscript -script 2014/crawler/crawler.m && echo "CVPR 2014 done!"
 wolframscript -script 2015/crawler/crawler.m && echo "CVPR 2015 done!"
 wolframscript -script 2016/crawler/crawler.m && echo "CVPR 2016 done!"
 wolframscript -script 2017/crawler/crawler.m && echo "CVPR 2017 done!"
+cd .. && cd ICCV && echo ""
+wolframscript -script 2013/crawler/crawler.m && echo "ICCV 2013 done!"
+wolframscript -script 2015/crawler/crawler.m && echo "CVPR 2015 done!"
+wolframscript -script 2017/crawler/crawler.m && echo "CVPR 2017 done!"
+cd ..
