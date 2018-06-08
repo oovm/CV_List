64ed981d30834f5e1c1ce27789d8e8d628b5b7be
GalAster <galaster@foxmail.com>
2018-06-08 15:13:52 +0800

    :mag_right:list updated

---
+++ Script/Auto-Readme.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: Readme-StringTemplate *)
+(* :Context: Readme-StringTemplate` *)
+(* :Author: Aster *)
+(* :Date: 2018-06-08 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2018 Aster *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["Readme-StringTemplate`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
